987d78221899155e8f324f5ece033e77cef58836
MuraliMohan <Muralik1403@gmail.com>
2023-11-30 21:50:08 +0530

    File Modifiled

--- sub.java
+++ sub.java
@@ -3,8 +3,8 @@ public class SubtractTwoNumbers {
     public static void main(String[] args) {
          
         // declare and initialize two integer numbers
-        int x=70;
-        int y=40;
+        int x=10;
+        int y=20;
  
         // declare another variable to hold the substracted value
         int subtract;
